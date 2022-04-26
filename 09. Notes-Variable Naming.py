3965a855884840b6d2d55bdd672ec2a67062ad9a
CodeNooby <35343129+Ridefast@users.noreply.github.com>
2022-04-26 16:06:16 +0200

    Add files via upload

---
+++ 09. Notes-Variable Naming.py
@@ -0,0 +1,25 @@
+Notes: Variable Naming
+
+-               Variables always need a Name. Something that is recognizable. "user_name" would be agood variable 
+                name for a Username Variable. "user name" would process a Error since is not allowed to use
+                spaces in Variable Names. 
+                
+Example:        user name = user_name
+                car list = car_list
+                bike parts = bike_parts
+                
+                Remember Variable Name are Case Sensitive and need to be correct. 
+                We can't have a list called "names" and then print a list called "namas". This will only work if 
+                there is a list called "namas". Name Errors mostly come from spell errors. 
+                
+-               Numbers in variable names are ok. But you we not allowed to use the Number at the bekginning
+                of the Variable name. "1length" would not be ok but "length1" would be ok. 
+              
+    Example:    user name 1 = user_name1
+                car list 1 = car_list1
+                bike parts 1 = bike_parts1
+                
+-               Privileged Words like "print or "input" should not be used as Vartiable Names. 
+
+Info Source:    https://www.w3schools.com/python/python_variables_names.asp       
+               
---
+++ 10. Day 01 Project - Band Name Generator.py
@@ -0,0 +1,9 @@
+print("Hello and Welcome\nto my first Python\nprogrammed Project")
+#Greetings
+a=input("Let me ask you a Question\nIn What city you grew up? \n")
+#Variable named "a" with a "\n" at the End to get the Cursor to a new Line
+b=input("What is you pets name? \n")
+#Variable named "b" with a "\n" at the End to get the Cursor to a new Line
+c=("The Name of your new Rockband is\n")
+#Variable "c" 
+print(c + a + " " + b + " Rockers")
